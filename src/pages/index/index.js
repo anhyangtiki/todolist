8526ad79434cc6657879f09984feb08f8b4a1179
anhyangtiki <109953529+anhyangtiki@users.noreply.github.com>
2022-07-29 14:42:33 +0700

    Add Storage API

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,27 +1,66 @@
-Tasks = []
 Page({
-    data: {
-        tasks: [],
-        inputTask: ''
-    },
-    onLoad(query) {},
-    onReady() {},
-    onShow() {},
-    onHide() {},
-    onUnload() {},
-    deleteTask(e) {
-        Tasks.splice(e.currentTarget.dataset.index, 1);
+  data: {
+    tasks: [],
+    inputTask: "",
+    keyList: "todolist-yang"
+  },
+  onLoad(query) {
+    my.getStorage({
+      key: this.data.keyList,
+      success: (res) => {
         this.setData({
-            tasks: Tasks
-        })
-    },
-    onInput(e) {
-        this.inputTask = e.detail.value
-    },
-    addTask(e) {
-        Tasks.push(this.inputTask)
-        this.setData({
-            tasks: Tasks
-        })
-    },
+          tasks: res.data.tasks
+        });
+      }
+    });
+  },
+  onReady() {},
+  onShow() {},
+  onHide() {},
+  onUnload() {},
+  deleteTask(e) {
+    this.data.tasks.splice(e.currentTarget.dataset.index, 1);
+    this.setData({
+      tasks: this.data.tasks
+    })
+    my.setStorage({
+      key: this.data.keyList,
+      data: {
+        tasks: this.data.tasks
+      }
+    })
+  },
+  onInput(e) {
+    this.data.inputTask = e.detail.value;
+  },
+  addTask(e) {
+    if (this.data.inputTask==="") return;
+    this.data.tasks = this.data.tasks || [];
+    this.data.tasks.push({
+      task: this.data.inputTask,
+      completed: false
+    })
+    this.setData({
+      tasks: this.data.tasks
+    })
+    my.setStorage({
+      key: this.data.keyList,
+      data: {
+        tasks: this.data.tasks
+      }
+    })
+  },
+  completedTask(e) {
+    this.data.tasks[e.currentTarget.dataset.index].completed = !this.data.tasks[e.currentTarget.dataset.index].completed
+    this.setData({
+      tasks: this.data.tasks
+    })
+    my.setStorage({
+      key: this.data.keyList,
+      data: {
+        tasks: this.data.tasks
+      }
+    })
+    console.log(this.data.tasks[e.currentTarget.dataset.index])
+  }
 });
